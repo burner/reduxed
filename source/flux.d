869f10d6025d232dcbf87e662bf9bb757354da32
Robert burner Schadek <robert.schadek@informatik.uni-oldenburg.de>
2018-07-13 01:00:05 +0200

    some testing

--- source/flux.d
+++ source/flux.d
@@ -10,3 +10,30 @@ unittest {
 
 	Flux!SomeStore f;
 }
+
+unittest {
+	import observable;
+	struct Bar {
+		Observable!float a;
+		Observable!float b;
+	}
+
+	struct Foo {
+		Observable!int a;
+		Observable!int b;
+
+		Bar bar;
+	}
+
+	struct Flux {
+		Foo foo;
+	}
+
+	void fun(ref const(float) f) @safe {
+		int a = 10;
+	}
+
+	Flux f;
+
+	f.foo.bar.a.subscribe(&fun);
+}
