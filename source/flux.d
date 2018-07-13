644c3e31597c760a6863fc57f245a3aded79dc84
Robert burner Schadek <rburners@gmail.com>
2018-07-13 15:03:27 +0200

    started working on the Flux struct

--- source/flux.d
+++ source/flux.d
@@ -1,14 +1,62 @@
 module flux;
 
-struct Flux(S) {
-}
+struct Flux(T) {
+	import observable;
 
-unittest {
-	struct SomeStore {
-		int value = 10;
+	static string indentString(size_t indent) pure {
+		string ret;
+		foreach(it; 0 .. indent) {
+			ret ~= "\t";
+		}
+		return ret;
 	}
 
-	Flux!SomeStore f;
+	static string buildStructImpl(S, size_t indent)() {
+		import std.traits : isBasicType;
+		import std.format : format;
+
+		string ret;
+		if(indent) {
+			ret = indentString(indent) ~ format("struct __Flux%s {\n", S.stringof);
+		}
+
+		static assert(is(S == struct));
+		foreach(mem; __traits(allMembers, S)) {
+			alias Type = typeof(__traits(getMember, S, mem));
+			enum isStruct = is(Type == struct);
+			static if(isStruct) {
+				ret ~= buildStructImpl!(Type, indent + 1)();
+			}
+			static if(isStruct) {
+				ret ~= indentString(indent + 1) ~ format("__Flux%s %s;\n", 
+							Type.stringof, mem
+						);
+			} else {
+				ret ~= indentString(indent + 1) ~ format("Observable!(%s) %s;\n", 
+							Type.stringof, mem
+						);
+			}
+		}
+
+		if(indent) {
+			ret ~= indentString(indent) ~ "}\n";
+		}
+
+		return ret;
+	}
+
+	static string buildStruct(S)() {
+		string ret = "struct Store {\n";
+		ret ~= buildStructImpl!(S, 0);
+
+		ret ~= "}\n";
+		ret ~= "Store store;\n";
+		ret ~= "alias store this;\n";
+		return ret;
+	}
+
+	mixin(buildStruct!T());
+	pragma(msg, buildStruct!T());
 }
 
 unittest {
@@ -37,3 +85,31 @@ unittest {
 
 	f.foo.bar.a.subscribe(&fun);
 }
+
+unittest {
+	struct Bar {
+		float a;
+		float b;
+	}
+
+	Flux!Bar store;
+}	
+
+unittest {
+	void fun(ref const(float) f) @safe {
+		int a = 10;
+	}
+
+	struct Foo {
+		float c;
+	}
+	struct Bar {
+		float a;
+		float b;
+		Foo foo;
+	}
+
+	Flux!Bar store;
+
+	store.foo.c.subscribe(&fun);
+}	
