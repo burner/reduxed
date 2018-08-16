aa04d343fc62831a0fa31d7589ab97c082924f8d
Robert burner Schadek <robert.schadek@informatik.uni-oldenburg.de>
2018-08-16 23:23:31 +0200

    unittestperf

--- source/fluxtests.d
+++ source/fluxtests.d
@@ -17,3 +17,32 @@ unittest {
 
 	store.foo.subscribe(&fun);
 }	
+
+unittest {
+	class Fun {
+		int localInt;
+
+		void callback(ref const(int) a) @safe {
+			this.localInt = a;
+		}
+	}
+
+	struct Store {
+		int a;
+	}
+
+	static int increment(int old) {
+		return old + 1;
+	}
+
+	auto f = new Fun();
+	assert(f.localInt == 0);
+
+	FluxStore!Store store;
+	store.a.subscribe(&f.callback);
+
+	foreach(i; 1 .. 10) {
+		store.execute!(increment)(store.a);
+		assert(f.localInt == i);
+	}
+}
