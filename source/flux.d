1878b82d2916a4e5fcc5896eb61fe7540799d886
Robert burner Schadek <robert.schadek@informatik.uni-oldenburg.de>
2018-08-21 22:55:13 +0200

    example

--- source/flux.d
+++ source/flux.d
@@ -149,29 +149,39 @@ unittest {
 }
 
 unittest {
-	import std.format : format;
+	// Define your store as a struct
 	struct Bar {
 		int b;
 		int a;
 	}
 
-	static int increment(int a, int b) {
-		return a + b; 
-	}
-
+	// Create the store
 	FluxStore!Bar store;
+	// set store init values
 	store.a = 1;
 	store.b = 2;
 
 	int a = 1337;
 
+	// We use this function to observe Bar.a
 	void fun(ref const(int) f) @safe {
 		a = f;
 	}
 
+	// Here we subscribe to Bar.a
+	// Whenever Bar.a changes "fun" gets called
 	store.a.subscribe(&fun);
 
+	// This function is used to change the value
+	static int increment(int a, int b) pure {
+		return a + b; 
+	}
+
+	// Here we call increment with the values of store.a and store.b
+	// the effect is equal to
+	// store.a = increment(store.a, store.b);
 	store.execute!(increment)(store.a, store.b);
-	assert(store.a.value == 3, format("%s", store.a.value));
-	assert(a == 3, format("%s", a));
+
+	assert(store.a.value == 3);
+	assert(a == 3);
 }	
